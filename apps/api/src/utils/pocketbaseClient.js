15874857b7fb6f96f89afedd78f61175d9832a80
GabrielGZK1 <gahdrum@gmail.com>
2026-04-03 13:42:11 -0300

    chore: preparar ambiente para deploy em vps easypanel

--- apps/api/src/utils/pocketbaseClient.js
+++ apps/api/src/utils/pocketbaseClient.js
@@ -2,7 +2,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 import Pocketbase from 'pocketbase';
 
-const pocketbaseClient = new Pocketbase('http://localhost:8090');
+const pocketbaseUrl = process.env.POCKETBASE_URL || 'http://localhost:8090';
+
+const pocketbaseClient = new Pocketbase(pocketbaseUrl);
 
 await pocketbaseClient.collection('_superusers').authWithPassword(
     process.env.PB_SUPERUSER_EMAIL,
